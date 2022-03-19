920315c3d69a565ed39801b85c40153fd33a391b
ΡΞΛΚSΤΞΡ <62203463+pkstDev@users.noreply.github.com>
2022-03-19 14:28:50 +0800

    Initial commit™ hahayes

---
+++ README.md
@@ -0,0 +1,13 @@
+# BANG
+
+*A.K.A. Benchworking Aesthetic Noise Generator I think*
+
+> **This mod is open source and under a permissive license.** As such, it can be included in any modpack on any platform without prior permission. We appreciate hearing about people using my mods, but you do not need to ask to use them. See the [LICENSE file](LICENSE) for more details.
+
+Simply added crafting sounds.
+
+They are played when a specific item is crafted. You can also define new items via tags (Defined in bang:carpentry/infusion/masonry/metalworking).
+
+#### Forge port plz?
+
+~~no f**k u~~ yea this should be very easy to port such single-mixin mod, but im just tooooooooo lazy to do so. do it urself plz
---
+++ src/main/java/committee/nova/pkst/bang/mixin/ItemStackMixin.java
@@ -0,0 +1,30 @@
+package committee.nova.pkst.bang.mixin;
+
+import committee.nova.pkst.bang.BANG;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.tag.TagKey;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+    @Shadow public abstract boolean isIn(TagKey<Item> tag);
+
+    @Inject(method = "onCraft", at = @At("RETURN"))
+    public void playSoundOnCraft(World world, PlayerEntity player, int amount, CallbackInfo ci) {
+        if (!world.isClient) {
+            if (this.isIn(BANG.CARPENTRY)) world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1f, 1f);
+            if (this.isIn(BANG.METALWORKING)) world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 1f, 1f);
+            if (this.isIn(BANG.INFUSION)) world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1f, 1f);
+            if (this.isIn(BANG.MASONRY)) world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BLOCK_STONE_BREAK, SoundCategory.BLOCKS, 1f, 1f);
+        }
+    }
+}
